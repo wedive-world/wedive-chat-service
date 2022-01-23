8996d197d87368911841c7e3f1f1385b58f4c388
Mark Yun <only.hyeok@gmail.com>
2022-01-23 17:16:14 +0900

    fixed updateUser

--- graphql/chat-user/chat-user.resolver.js
+++ graphql/chat-user/chat-user.resolver.js
@@ -8,7 +8,7 @@ module.exports = {
             let chatUsers = []
             if (parent.userIds && parent.userIds.length > 0) {
                 for (userId of parent.userIds) {
-                    let chatUser = await getChatUserById(userId)
+                    let chatUser = await getChatUserByUsername(userId)
                     chatUsers.push(chatUser)
                 }
             }
@@ -23,7 +23,7 @@ module.exports = {
 
     ChatMessage: {
         async author(parent, args, context, info) {
-            return await getChatUserById(parent.author)
+            return await getChatUserByUsername(parent.author)
         },
     },
 
@@ -31,7 +31,7 @@ module.exports = {
         async getChatUserById(parent, args, context, info) {
 
             console.log(`query | getChatUserById: context=${JSON.stringify(context)}`)
-            return await getChatUserById(context.uid)
+            return await getChatUserByUserName(context.uid)
         },
 
     },
@@ -48,8 +48,10 @@ module.exports = {
         async updateChatUser(parent, args, context, info) {
 
             console.log(`mutation | updateChatUser: args=${JSON.stringify(args)}`)
-            let user = await updateUser(context.uid, args.name, args.avatarUrl)
-            return user
+            let user = await getChatUserByUserName(context.uid)
+            console.log(`mutation | updateChatUser: user=${JSON.stringify(user)}`)
+            let chatUser = await updateUser(user._id, context.uid, args.name, args.avatarUrl)
+            return chatUser
         },
 
         async updateFcmToken(parent, args, context, info) {
@@ -61,10 +63,10 @@ module.exports = {
     },
 };
 
-async function getChatUserById(_id) {
+async function getChatUserByUserName(userName) {
 
     let queryParams = {
-        username: _id
+        username: userName
     }
 
     let result = await client.get('/api/v1/users.info', client.getAixosAdminHeader(), queryParams)
@@ -119,35 +121,44 @@ async function createUser(_id, email, name) {
     return convertUser(result.user)
 }
 
-async function updateUser(uid, name, avatarUrl) {
+async function updateUser(userId, uid, name, avatarUrl) {
+    console.log(`chat-user-service | updateUser: userId=${userId}, uid=${uid}, name=${name}, avatarUrl=${avatarUrl}}`)
 
-    let avatarData = {
-        userId: uid,
-        avatarUrl: avatarUrl
-    }
+    let success = true
+    let reason = ''
 
-    let avatarResult = await client.post('/api/v1/users.setAvatar', client._getUserToken(uid), avatarData)
-    if (!avatarResult.success) {
-        console.log(`chat-user-service | updateUser: failed, avatarResult=${JSON.stringify(avatarResult)}`)
-    }
+    if (avatarUrl) {
 
-    let userData = {
-        userId: uid,
-        data: {
-            name: name
+        let avatarData = {
+            userId: userId,
+            avatarUrl: avatarUrl
+        }
+
+        let avatarResult = await client.post('/api/v1/users.setAvatar', await client.generateUserHeader(uid), avatarData)
+        if (!avatarResult.success) {
+            console.log(`chat-user-service | updateUser: failed, avatarResult=${JSON.stringify(avatarResult)}`)
+            reason += avatarResult
         }
     }
 
-    let userResult = await client.post('/api/v1/users.update', client._getUserToken(uid), userData)
-    if (!userResult.success) {
-        console.log(`chat-user-service | updateUser: failed, userResult=${JSON.stringify(userResult)}`)
+    if (name) {
+        let userData = {
+            // userId: userId,
+            data: {
+                name: name
+            }
+        }
+
+        let userResult = await client.post('/api/v1/users.updateOwnBasicInfo', await client.generateUserHeader(uid), userData)
+        if (!userResult.success) {
+            console.log(`chat-user-service | updateUser: failed, userResult=${JSON.stringify(userResult)}`)
+            reason += userResult
+        }
     }
 
-    console.log(`updateUser: avatarResult=${JSON.stringify(avatarResult)}`)
-    console.log(`updateUser: userResult=${JSON.stringify(userResult)}`)
-
     return {
-        success: avatarResult.success && userResult.success
+        success: true,
+        reason: reason
     }
 }
 
